773eaf5afcf4927bc689f313f8b92d59b5ae7932
George <george@georgekaplan.xyz>
2018-03-04 00:31:36 -0500

    reading and splitting files

--- scrabbler.go
+++ scrabbler.go
@@ -2,6 +2,8 @@ package main
 
 import "os"
 import "fmt"
+import "bufio"
+import "strings"
 import "io/ioutil"
 import "encoding/json"
 
@@ -15,11 +17,20 @@ func main() {
 		panic(err)
 	}
 	json.Unmarshal(rawScores, &scoresByLang)
+
 	// load text from file
 	readFile := os.Args[1]
 	rawText, err := ioutil.ReadFile(readFile)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(rawText)
+	var words []string
+	scanner := bufio.NewScanner(strings.NewReader(string(rawText)))
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	fmt.Println(words)
 }
